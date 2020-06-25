5bf63285b6b43169dcc54dde1b906ead0eee949c
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-25 14:46:43 -0300

    JS Units test feito

---
+++ project-js-unit-tests/projeto-feito/src/average.js
@@ -0,0 +1,29 @@
+/*
+  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  Caso a função receba algum valor não númerico ou um array vazio,
+  o valor undefined deve ser retornado.
+  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+
+  Parâmetros:
+    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
+  Comportamento:
+    - average([2, 2]) // Retorno: 2;
+    - average([1, 2]) // Retorno: 1;
+    - average([1, '2']) // Retorno: undefined;
+*/
+
+const average = (arr) => {
+  let parameterSoma = 0;
+  let media;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof (arr[i]) === 'number') {
+      parameterSoma += arr[i];
+      media = Math.round((parameterSoma / arr.length) - 0.0001);
+    } else {
+      return undefined;
+    }
+  }
+  if (media === -0) media = 0;
+  return media;
+};
+module.exports = average;
